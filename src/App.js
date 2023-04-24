937543bc6d5c7d9f3a2789d5f5d5bf86c47f4f3c
vidurgupta99 <123901820+vidurgupta99@users.noreply.github.com>
2023-04-23 22:21:12 -0400

    Updated Source Folder

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios';
+import PostIt from './components/PostIt';
+import Add from './components/Add';
+import Edit from './components/Edit'
+import { Button } from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+const App = () => {
+
+  const [PostIts, SetPostIts] = useState([])
+
+  const handleCreate = (data) => {
+
+    axios.post('http://localhost:3000/PostIts', data)
+    .then((response) => {
+      let newPostIts = [...postits, response.data]
+      setAnimals(newPostIts)
+    })
+  }
+
+    const handleEdit = (data) => {
+    axios.put('http://localhost:3000/postits/' + data._id, data)
+    .then((response) => {
+      let newPostIts = postits.map((postit) => {
+        return newPostIts._id !== data._id ? newPostIts : data
+      })
+      setAnimals(newPostIts)
+    })
+  }
+
+  const handleDelete = (deletedPostIt) => {
+    axios.delete('http://localhost:3000/postits/' + deletedAnimal._id)
+    .then((response) => {
+      let newPostIts = postit.filter((postits) => {
+        return postit._id !== deletedPostIt._id
+      })
+      SetPostIts(newPostIts)
+    })
+  }
+
+  const getPostIts = () => {
+    axios.get('http://localhost:3000/postits')
+    .then((response) => setAnimals(response.data), (err) => console.log(err))
+    .catch((error) => console.log(error))
+  }
+
+  useEffect(() => {
+    getPostIts()
+  })
+
+  return (
+    <div>
+      <h2 className='font'>Post Its</h2>
+
+      <Add handleCreate={handleCreate}/>
+
+      {postits.map((PostIt) => {
+        return (
+          <>
+            <PostIt postit={postit} handleEdit={handleEdit}/>
+            <Edit postit={postit} handleEdit={handleEdit}/>
+            <Button variant='btn btn-outline-info' onClick={ () => {handleDelete(postit) } }>Remove Post It</Button>
+          </>
+        )
+      })}
+    </div>
+  );
+}
+
+export default App;
